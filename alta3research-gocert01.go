1452c5815e68a8a01d435b7dbd36bb6119ccff8f
indresh padmonkar <indreshp@users.noreply.github.com>
2022-02-12 13:00:51 +0000

    Research Go certification project

---
+++ alta3research-gocert01.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "io/ioutil"
+)
+
+func main() {
+
+   url := "https://api.nasa.gov"
+   method := "GET"
+   client := &http.Client{}
+
+   req, err := http.NewRequest(method, url, nil)
+
+   if err != nil {
+      fmt.Println(err)
+   }
+
+   res, err := client.Do (req)
+   if err != nil{
+        fmt.Println(err)
+        return
+    }
+    defer res.Body.Close()
+
+    body, err := ioutil.ReadAll(res.Body)
+
+    if err != nil{
+       fmt.Println(err)
+    }
+
+    fmt.Println(string(body))
+
+}
