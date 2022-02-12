172501cd74cefe48a0d92be8f3891568a0bc73f1
indresh padmonkar <indreshp@users.noreply.github.com>
2022-02-12 23:53:45 +0000

    Research Go certification project

--- alta3research-gocert01.go
+++ alta3research-gocert01.go
@@ -3,17 +3,38 @@ package main
 import (
   "fmt"
   "net/http"
-  "io/ioutil"
+  "encoding/json"
+  "log"
 )
 
+//type MarsPhoto []struct {
+type	MarsPhotos []struct {
+		ID     int `json:"id"`
+		Sol    int `json:"sol"`
+		Camera struct {
+			ID       int    `json:"id"`
+			Name     string `json:"name"`
+			RoverID  int    `json:"rover_id"`
+			FullName string `json:"full_name"`
+		} `json:"camera"`
+		ImgSrc    string `json:"img_src"`
+		EarthDate string `json:"earth_date"`
+		Rover     struct {
+			ID          int    `json:"id"`
+			Name        string `json:"name"`
+			LandingDate string `json:"landing_date"`
+			LaunchDate  string `json:"launch_date"`
+			Status      string `json:"status"`
+		} `json:"rover"`
+	} // `json:"photos"`
+//}
+ 
 func main() {
-
-   url := "https://api.nasa.gov"
+    url := "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=2&api_key=DEMO_KEY"
    method := "GET"
    client := &http.Client{}
 
    req, err := http.NewRequest(method, url, nil)
-
    if err != nil {
       fmt.Println(err)
    }
@@ -24,13 +45,22 @@ func main() {
         return
     }
     defer res.Body.Close()
+    //fmt.Println(res.Body)
 
-    body, err := ioutil.ReadAll(res.Body)
+    var record  MarsPhotos // MarsPhoto
+    if err := json.NewDecoder(res.Body).Decode(&record); err != nil {
+                log.Println(err)
+        }
+     //fmt.Println(record)
+     
+     for ID := range record {
+        fmt.Println(ID)
+     }
 
-    if err != nil{
-       fmt.Println(err)
-    }
-
-    fmt.Println(string(body))
 
 }
+
+
+
+
+
